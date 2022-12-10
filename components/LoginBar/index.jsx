19405a93c41c9b1995ab3f6bb4b84889206de452
Dario <99050545+bluemountains88@users.noreply.github.com>
2022-12-10 12:41:58 -0300

    Update index.jsx

--- components/LoginBar/index.jsx
+++ components/LoginBar/index.jsx
@@ -1,5 +1,6 @@
 import { useUser } from '@auth0/nextjs-auth0';
 import Image from 'next/image';
+import Link from 'next/link';
 import styles from './LoginBar.module.css';
 
 const LoginBar = () => {
@@ -18,21 +19,21 @@ const LoginBar = () => {
           height="50"
           alt="profile picture"
         />
-        <a href="/api/auth/logout">
+        <Link href="/api/auth/logout">
           <svg xmlns="http://www.w3.org/2000/svg" height="48" width="48">
             <path d="m33.3 32.75-2.15-2.15 5.1-5.1h-17.5v-3h17.4l-5.1-5.1 2.15-2.15 8.8 8.8ZM6 42V6h17.55v3H9v30h14.55v3Z" />
           </svg>
-        </a>
+        </Link>
       </div>
     );
   }
   return (
     <div className={styles.bar}>
-      <a href="/api/auth/login">
+      <Link href="/api/auth/login">
         <svg xmlns="http://www.w3.org/2000/svg" height="48" width="48">
           <path d="M20.55 32.75 18.4 30.6l5.1-5.1H6v-3h17.4l-5.1-5.1 2.15-2.15 8.8 8.8Zm3.9 9.25v-3H39V9H24.45V6H42v36Z" />
         </svg>
-      </a>
+      </Link>
     </div>
   );
 };
