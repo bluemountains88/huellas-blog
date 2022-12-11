82a8c7b3d3978dba85fd8d24e62067d87342efb4
bluemountains88 <m.dariouk@gmail.com>
2022-12-10 22:24:48 -0300

    fix

--- components/LoginBar/index.jsx
+++ components/LoginBar/index.jsx
@@ -1,7 +1,26 @@
+/* ignore eslint */
 import { useUser } from '@auth0/nextjs-auth0';
 import Image from 'next/image';
-import Link from 'next/link';
 import styles from './LoginBar.module.css';
+
+const links = [
+  {
+    label: (
+      <svg xmlns="http://www.w3.org/2000/svg" height="48" width="48">
+        <path d="m33.3 32.75-2.15-2.15 5.1-5.1h-17.5v-3h17.4l-5.1-5.1 2.15-2.15 8.8 8.8ZM6 42V6h17.55v3H9v30h14.55v3Z" />
+      </svg>
+    ),
+    route: 'https://www.somoshuella.com.ar/api/auth/logout',
+  },
+  {
+    label: (
+      <svg xmlns="http://www.w3.org/2000/svg" height="48" width="48">
+        <path d="M20.55 32.75 18.4 30.6l5.1-5.1H6v-3h17.4l-5.1-5.1 2.15-2.15 8.8 8.8Zm3.9 9.25v-3H39V9H24.45V6H42v36Z" />
+      </svg>
+    ),
+    route: 'https://www.somoshuella.com.ar/api/auth/login',
+  },
+];
 
 const LoginBar = () => {
   const { user, error, isLoading } = useUser();
@@ -15,25 +34,17 @@ const LoginBar = () => {
       <div className={styles.bar}>
         <Image
           src={user.picture}
-          width="50"
-          height="50"
+          width="40"
+          height="40"
           alt="profile picture"
         />
-        <Link href="https://www.somoshuella.com.ar/api/auth/logout">
-          <svg xmlns="http://www.w3.org/2000/svg" height="48" width="48">
-            <path d="m33.3 32.75-2.15-2.15 5.1-5.1h-17.5v-3h17.4l-5.1-5.1 2.15-2.15 8.8 8.8ZM6 42V6h17.55v3H9v30h14.55v3Z" />
-          </svg>
-        </Link>
+        <a href={links[0].route}>{links[0].label}</a>
       </div>
     );
   }
   return (
     <div className={styles.bar}>
-      <Link href="https://www.somoshuella.com.ar/api/auth/login">
-        <svg xmlns="http://www.w3.org/2000/svg" height="48" width="48">
-          <path d="M20.55 32.75 18.4 30.6l5.1-5.1H6v-3h17.4l-5.1-5.1 2.15-2.15 8.8 8.8Zm3.9 9.25v-3H39V9H24.45V6H42v36Z" />
-        </svg>
-      </Link>
+      <a href={links[1].route}>{links[1].label}</a>
     </div>
   );
 };
