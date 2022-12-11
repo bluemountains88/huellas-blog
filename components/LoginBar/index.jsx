9260b4fc06087260f15ecfb755d85207c6af3765
bluemountains88 <m.dariouk@gmail.com>
2022-12-10 22:31:07 -0300

    fix

--- components/LoginBar/index.jsx
+++ components/LoginBar/index.jsx
@@ -1,23 +1,25 @@
-/* ignore eslint */
 import { useUser } from '@auth0/nextjs-auth0';
 import Image from 'next/image';
+import Link from 'next/link';
 import styles from './LoginBar.module.css';
 
 const links = [
   {
-    label: (
+    icon: (
       <svg xmlns="http://www.w3.org/2000/svg" height="48" width="48">
         <path d="m33.3 32.75-2.15-2.15 5.1-5.1h-17.5v-3h17.4l-5.1-5.1 2.15-2.15 8.8 8.8ZM6 42V6h17.55v3H9v30h14.55v3Z" />
       </svg>
     ),
+    label: 'Logout',
     route: 'https://www.somoshuella.com.ar/api/auth/logout',
   },
   {
-    label: (
+    icon: (
       <svg xmlns="http://www.w3.org/2000/svg" height="48" width="48">
         <path d="M20.55 32.75 18.4 30.6l5.1-5.1H6v-3h17.4l-5.1-5.1 2.15-2.15 8.8 8.8Zm3.9 9.25v-3H39V9H24.45V6H42v36Z" />
       </svg>
     ),
+    label: 'Login',
     route: 'https://www.somoshuella.com.ar/api/auth/login',
   },
 ];
@@ -38,13 +40,13 @@ const LoginBar = () => {
           height="40"
           alt="profile picture"
         />
-        <a href={links[0].route}>{links[0].label}</a>
+        <Link href={links[0].route}>{links[0].label}</Link>
       </div>
     );
   }
   return (
     <div className={styles.bar}>
-      <a href={links[1].route}>{links[1].label}</a>
+      <Link href={links[1].route}>{links[1].label}</Link>
     </div>
   );
 };
